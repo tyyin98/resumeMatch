1bccbfe5adc96f6768977ce7d4f970d4165f1dbf
Tianyi Yin <tianyiy.ubc@gmail.com>
2024-07-10 22:20:01 -0700

    nav layout lg screen

--- app/page.tsx
+++ app/page.tsx
@@ -23,46 +23,41 @@ export default async function Index() {
   const isSupabaseConnected = canInitSupabaseClient();
 
   return (
-    <div>
-      {/* <Head>
-        <link rel="icon" href="/favicon.ico" />
-      </Head> */}
-      <div className="flex-1 w-full flex flex-col gap-20 items-center">
-        <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-          <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-            <Logo />
-            {isSupabaseConnected && <AuthButton />}
-          </div>
-        </nav>
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <Logo />
+          {isSupabaseConnected && <AuthButton />}
+        </div>
+      </nav>
 
-        <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
-          <Header />
-          <main className="flex-1 flex flex-col gap-6">
-            <Link className="text-center text-4xl font-bold" href="/protected">
-              Start
-            </Link>
-            {/* {isSupabaseConnected ? (
+      <div className="flex-1 flex flex-col gap-20 max-w-4xl px-3">
+        <Header />
+        <main className="flex-1 flex flex-col gap-6">
+          <Link className="text-center text-4xl font-bold" href="/protected">
+            Start
+          </Link>
+          {/* {isSupabaseConnected ? (
               <SignUpUserSteps />
             ) : (
               <ConnectSupabaseSteps />
             )} */}
-          </main>
-        </div>
-
-        <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
-        </footer>
+        </main>
       </div>
+
+      <footer className="w-full border-t border-t-foreground/10 p-8 flex justify-center text-center text-xs">
+        <p>
+          Powered by{" "}
+          <a
+            href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
+            target="_blank"
+            className="font-bold hover:underline"
+            rel="noreferrer"
+          >
+            Supabase
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
