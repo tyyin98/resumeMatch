465b0cead271a556363fb998edc0dcfc72140ef1
Tianyi Yin <tianyiy.ubc@gmail.com>
2024-07-10 22:47:43 -0700

    seamless Loading state

--- app/protected/loading.tsx
+++ app/protected/loading.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import Logo from "@/components/Logo";
 
 export default function loading() {
-  return <div>loading</div>;
+  return (
+    <div className="flex-1 w-full flex flex-col gap-20 items-center">
+      <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
+          <Logo />
+        </div>
+      </nav>
+      <div className="flex-1 w-full flex flex-col gap-20 items-center">
+        LOADING...
+      </div>
+    </div>
+  );
 }
