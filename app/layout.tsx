4dc775e2e20cb364dab11424f9c43ddfacda7d5f
Tianyi Yin <tianyiy.ubc@gmail.com>
2024-07-14 15:26:22 -0700

    added  <Analytics />

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,7 @@ export default function RootLayout({
       <body className="bg-background text-foreground">
         <main className="min-h-screen flex flex-col items-center">
           {children}
+          <Analytics />
         </main>
       </body>
     </html>
